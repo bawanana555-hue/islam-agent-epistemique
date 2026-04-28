d10b2e6029f311dad4520d94c5cc508134e649e6
bawanana555-hue <bawanana555@gmail.com>
2026-04-28 17:59:47 +0000

    Add service worker for caching and fetch handling
    Implement service worker for caching assets and handling fetch requests.

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,43 @@
 
+*-worker.js
+
+```js
+const CACHE_NAME = "iae-v2";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", function(event) {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names.filter(function(n) { return n !== CACHE_NAME; })
+             .map(function(n) { return caches.delete(n); })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+```
+
+ 
